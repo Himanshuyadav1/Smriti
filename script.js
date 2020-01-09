f6e4fca81a2c5dd40505f1ef349258cef49edb3e
Himanshuyadav1 <himanshu.tj@gmail.com>
2020-01-09 23:38:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Smriti" && b=="10-01-1992")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
